2b81e8704f10441285a0f694acc29c93a992bfd6
ruanyandong <729368173@qq.com>
2018-05-28 23:09:46 +0800

    android时间轴

---
+++ timeline2/src/main/java/com/example/timeline2/TraceListAdapter.java
@@ -0,0 +1,84 @@
+package com.example.timeline2;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class TraceListAdapter extends BaseAdapter {
+    private Context context;
+    private List<Trace> traceList;
+    private static final int TYPE_TOP = 0x0000;
+    private static final int TYPE_NORMAL= 0x0001;
+
+    public TraceListAdapter(Context context, List<Trace> traceList) {
+        this.context = context;
+        this.traceList = traceList;
+    }
+
+    @Override
+    public int getCount() {
+        return traceList.size();
+    }
+
+    @Override
+    public Trace getItem(int position) {
+        return traceList.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ViewHolder holder;
+        final Trace trace = getItem(position);
+        if (convertView != null) {
+            holder = (ViewHolder) convertView.getTag();
+        } else {
+            holder = new ViewHolder();
+            convertView = LayoutInflater.from(context).inflate(R.layout.time_line_item, parent, false);
+            holder.tvAcceptTime = (TextView) convertView.findViewById(R.id.tvAcceptTime);
+            holder.tvAcceptStation = (TextView) convertView.findViewById(R.id.tvAcceptStation);
+            holder.tvTopLine = (TextView) convertView.findViewById(R.id.tvTopLine);
+            holder.tvDot = (TextView) convertView.findViewById(R.id.tvDot);
+            convertView.setTag(holder);
+        } if (getItemViewType(position) == TYPE_TOP) {
+            // 第一行头的竖线不显示
+            holder.tvTopLine.setVisibility(View.INVISIBLE);
+            // 字体颜色加深
+            holder.tvAcceptTime.setTextColor(0xff555555);
+            holder.tvAcceptStation.setTextColor(0xff555555);
+            holder.tvDot.setBackgroundResource(R.drawable.timelline_dot_first);
+        } else if (getItemViewType(position) == TYPE_NORMAL) {
+            holder.tvTopLine.setVisibility(View.VISIBLE);
+            holder.tvAcceptTime.setTextColor(0xff999999);
+            holder.tvAcceptStation.setTextColor(0xff999999);
+            holder.tvDot.setBackgroundResource(R.drawable.timelline_dot_normal);
+        }
+        holder.tvAcceptTime.setText(trace.getAcceptTime());
+        holder.tvAcceptStation.setText(trace.getAcceptStation());
+        return convertView;
+    }
+
+    @Override
+    public int getItemViewType(int id) {
+        if (id == 0) {
+            return TYPE_TOP;
+        }
+        return TYPE_NORMAL;
+    }
+
+    static class ViewHolder {
+        public TextView tvAcceptTime, tvAcceptStation;
+        public TextView tvTopLine, tvDot;
+    }
+
+}
